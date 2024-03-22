c8c1052c2baf12da3859e4d07fadbcf53a2586ca
Hacanabbas <163378374+Hacanabbas@users.noreply.github.com>
2024-03-22 21:34:43 +0500

    Delete classwork2 directory

--- classwork2/dom.js
+++
@@ -1,7 +0,0 @@
-function update()
-{
-    var input= document.getElementById('123')
-    console.log(input.value);
-    var output = document.getElementById('456')
-    output.innerHTML=input.value;
-}
